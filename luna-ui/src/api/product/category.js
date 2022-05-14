817044ad859922fce1ebe3952b30014e86914556
luna <iszychen@gmail.com>
2022-05-14 16:14:37 +0800

    utils

--- luna-ui/src/api/product/category.js
+++ luna-ui/src/api/product/category.js
@@ -119,3 +119,11 @@ export function delCategory(id) {
   })
 }
 
+// 条件逻辑删除产品分类
+export function delCategory(data) {
+  return request({
+    url: '/product/category/delete',
+    method: 'delete',
+    data: data
+  })
+}
